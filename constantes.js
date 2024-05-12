37556f95b69f24cdde8f6ff24f8d8ad8dc5c664c
JOSÉ-ANTONIO-HD <hdzdomjoseantonio@gmail.com>
2024-05-11 22:31:47 -0600

    base de datos

--- constantes.js
+++ constantes.js
@@ -1,12 +1,12 @@
-const DB_USER="danycambrano1";
-const DB_PASSWORD="ZvNIBIvKmk5RmjxD";
-const DB_HOST="productos.obvqzpn.mongodb.net";
-const DB_NAME="product";
+const DB_USER="INVENTAX";
+const DB_PASSWORD="xTrTkpxT0ivyuGRR";
+const DB_HOST="basededatos.fas1b8t.mongodb.net";
+const DB_NAME="BASEDEDATOS";
 
 //const IP_SERVER="187.154.139.32";
 const IP_SERVER="127.0.0.1";
 
-JWT_SECRET_KEY="palabrasecreta";
+JWT_SECRET_KEY="palabrasecreta123";
 
 module.exports={
     DB_HOST,
